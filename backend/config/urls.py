53eb57b6e5ea4a7ad5b4be37b05f91dcbf3b3dcc
saraali25 <sara.ali.mostafa@gmail.com>
2025-11-10 19:21:26 +0200

    Fix: remove frontend routes from Django

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,9 +7,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls')),
-    path('api/', include('courses.urls')),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include('users.urls')),
+    path("api/", include('courses.urls')),
 ]
 
 # Serve media files in development

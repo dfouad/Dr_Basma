7dd4adec276c246517b493e31f91dbbf8614baaa
saraali25 <sara.ali.mostafa@gmail.com>
2025-11-10 18:31:16 +0200

    modify allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,7 +21,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='https://drbasma-production.up.railway.app/,localhost,127.0.0.1').split(',')
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='https://drbasma-production.up.railway.app/,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["*", os.getenv("RAILWAY_PUBLIC_DOMAIN", "")]
 
 
 # Application definition

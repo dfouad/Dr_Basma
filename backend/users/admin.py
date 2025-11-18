ddd3d317f794491743768521e674afb73deaeb82
dfouad <eng.doaa.fouad@gmail.com>
2025-11-19 00:49:20 +0200

    fixing email verification (load_dotenv fix and prt 8080)

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
-
+from .models import PendingUser
 User = get_user_model()
 
 
@@ -27,3 +27,11 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'is_active'),
         }),
     )
+    
+
+
+@admin.register(PendingUser)
+class PendingUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'created_at', 'is_verified')
+    search_fields = ('email',)
+    list_filter = ('is_verified', 'created_at')

667df1b331476dd0a19b1655bd3bfeb70ece848e
saraali25 <sara.ali.mostafa@gmail.com>
2025-11-19 11:21:52 +0200

    try to delete db

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,8 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, "config", ".env"))
+#load_dotenv(os.path.join(BASE_DIR, "config", ".env"))
+load_dotenv(os.path.join(BASE_DIR,".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')

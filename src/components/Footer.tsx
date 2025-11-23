a855989743f8196c2bb5f762fb0c8dfef7e96fc0
saraali25 <sara.ali.mostafa@gmail.com>
2025-11-23 23:56:03 +0200

    f

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,15 +51,15 @@ const Footer = () => {
                 <Facebook className="h-5 w-5" />
                  </a>
               <button
-  type="button"
-  onClick={() =>
-    window.open("https://wa.me/201555676851", "_blank", "noopener,noreferrer")
-  }
-  aria-label="WhatsApp"
-  className="text-muted-foreground hover:text-primary transition-colors"
->
-  <FaWhatsapp className="h-5 w-5" />
-</button>
+                      type="button"
+                      onClick={() =>
+                        window.open("https://wa.me/message/IFEAWYSTJ2DUE1", "_blank", "noopener,noreferrer")
+                      }
+                      aria-label="WhatsApp"
+                      className="text-muted-foreground hover:text-primary transition-colors"
+                    >
+                      <FaWhatsapp className="h-5 w-5" />
+                    </button>
 
 
               <a

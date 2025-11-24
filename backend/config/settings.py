52c44c965db83f38997571df1a95a9b8546bcc65
saraali25 <sara.ali.mostafa@gmail.com>
2025-11-24 21:01:23 +0200

    test email

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -17,6 +17,9 @@ load_dotenv(os.path.join(BASE_DIR,".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
